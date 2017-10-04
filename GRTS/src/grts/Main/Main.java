9c8c067b178b2e42de605007f34f19405f4d0845
somar <somar@Extreme-PC.fritz.box>
2017-10-04 21:42:27 +0200

    123

--- GRTS/src/grts/Main/Main.java
+++ GRTS/src/grts/Main/Main.java
@@ -1,12 +1,17 @@
 package grts.Main;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Main Window!");
-		window.setContentPane(new MainPanel());
+		MainPanel MP = new MainPanel();
+		window.setLocationRelativeTo(null);
+		window.setPreferredSize(new Dimension(MP.WIDTH * MP.SCALE, MP.WIDTH * MP.SCALE));
+		window.setContentPane(MP);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.pack();
