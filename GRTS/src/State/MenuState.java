c1664f4570da48e26165ceae496db3bb5967c021
D065922 <D065922@WDFN33991635A.emea.global.corp.sap>
2017-10-04 14:59:45 +0200

    After adding the Background Class 25:34

---
+++ GRTS/src/State/MenuState.java
@@ -0,0 +1,30 @@
+package State;
+
+import java.awt.Graphics2D;
+
+import TileMap.Background;
+
+public class MenuState extends State {
+
+	private Background bg;
+
+	public MenuState(StateManager sm) {
+		this.sm = sm;
+	}
+
+	public void init() {
+	}
+
+	public void update() {
+	}
+
+	public void draw(Graphics2D g) {
+	}
+
+	public void keyPressed(int k) {
+	}
+
+	public void keyReleased(int k) {
+	}
+
+}
--- GRTS/src/State/StateManager.java
+++ GRTS/src/State/StateManager.java
@@ -2,39 +2,41 @@ package State;
 
 import java.util.ArrayList;
 
-
 public class StateManager {
-	
+
 	private ArrayList<State> states;
 	private int currentState;
-	
-	public StateManager (){
-		states = new ArrayList <State>();
-		
+
+	public static final int MENUSTATE = 0;
+	public static final int LEVEL1STATE = 1;
+
+	public StateManager() {
+		states = new ArrayList<State>();
+
 		currentState = MENUSTATE;
 		states.add(new MenuState(this));
 	}
-	
-	public void setState(int state){
-		
+
+	public void setState(int state) {
+
 		currentState = state;
 		states.get(currentState).init();
 	}
-	
-	public void update(){
+
+	public void update() {
 		states.get(currentState).update();
 	}
-	
-	public void draw(java.awt.Graphics2D g){
-		states.get(currentState).draw();
-			
+
+	public void draw(java.awt.Graphics2D g) {
+		states.get(currentState).draw(g);
+
 	}
-	
-	public void keyPressed(int k){
+
+	public void keyPressed(int k) {
 		states.get(currentState).keyPressed(k);
 	}
-	
-	public void keyReleased(int k){
+
+	public void keyReleased(int k) {
 		states.get(currentState).keyReleased(k);
 	}
 
