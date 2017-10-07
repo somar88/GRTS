64c857b4be34726ec5b3ffea24098ba1a7bd815f
somar <somar@Extreme-PC.fritz.box>
2017-10-07 23:37:23 +0200

    all

--- GRTS/src/State/Level1State.java
+++ GRTS/src/State/Level1State.java
@@ -1,8 +1,10 @@
 package State;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import TileMap.TileMap;
+import grts.Main.MainPanel;
 
 public class Level1State extends State {
 
@@ -10,12 +12,16 @@ public class Level1State extends State {
 
 	public Level1State(StateManager sm) {
 		this.sm = sm;
+		init();
 	}
 
 	@Override
 	public void init() {
 
-		// tileMap = new TileMap(30);
+		tileMap = new TileMap(30);
+		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadMap("Maps/level1-1.map");
+		tileMap.setPosition(0, 0);
 	}
 
 	@Override
@@ -24,6 +30,12 @@ public class Level1State extends State {
 
 	@Override
 	public void draw(Graphics2D g) {
+		// clear screen
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, MainPanel.WIDTH, MainPanel.HIGHT);
+
+		// draw tilemap
+		tileMap.draw(g);
 	}
 
 	@Override
