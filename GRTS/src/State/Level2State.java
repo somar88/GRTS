8efa7e763ab0f3928c7e45fd5839f511a3f7d85b
D065922 <D065922@WDFN33991635A.emea.global.corp.sap>
2017-10-09 16:31:08 +0200

    Running Help menu

---
+++ GRTS/src/State/Level2State.java
@@ -0,0 +1,106 @@
+package State;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import com.sun.glass.events.KeyEvent;
+import TileMap.Background;
+
+public class Level2State extends State {
+
+	private Background bg;
+
+	private int currentChoice = 0;
+
+	private String[] options = { "1st", "2nd", "3rd", "Back to Main Menu" };
+
+	private Color titleColor;
+	private Font titleFont;
+
+	private Font font;
+
+	public Level2State(StateManager sm) {
+		this.sm = sm;
+
+		try {
+			bg = new Background("/Backgrounds/menubg.gif", 1);
+			bg.setVector(0.6, 0);
+
+			titleColor = new Color(128, 0, 0);
+			titleFont = new Font("Century Gothic", Font.PLAIN, 28);
+
+			font = new Font("Arial", Font.PLAIN, 12);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void init() {
+	}
+
+	public void update() {
+		bg.update();
+	}
+
+	public void draw(Graphics2D g) {
+		// draw Background
+		bg.draw(g);
+
+		// draw Title
+		g.setColor(titleColor);
+		g.setFont(titleFont);
+		g.drawString("Second interface", 80, 70);
+
+		// Draw menu options
+		g.setFont(font);
+		for (int i = 0; i < options.length; i++) {
+			if (i == currentChoice) {
+				g.setColor(Color.BLACK);
+			} else {
+				g.setColor(Color.RED);
+			}
+			g.drawString(options[i], 145, 140 + i * 15);
+		}
+	}
+
+	private void select() {
+		if (currentChoice == 0) {
+			// Start
+			sm.setState(StateManager.MENUSTATE);
+		}
+		if (currentChoice == 1) {
+			// TODO help
+		}
+		if (currentChoice == 2) {
+		}
+		if (currentChoice == 3) {
+			// Go Back
+			sm.setState(StateManager.LEVEL1STATE);
+		}
+	}
+
+	public void keyPressed(int k) {
+		if (k == KeyEvent.VK_ENTER) {
+			select();
+		}
+		if (k == KeyEvent.VK_UP) {
+			currentChoice--;
+			if (currentChoice < 0) {
+				currentChoice = options.length - 1;
+			}
+		}
+		if (k == KeyEvent.VK_DOWN) {
+			currentChoice++;
+			if (currentChoice >= options.length) {
+				currentChoice = 0;
+			}
+		}
+
+	}
+
+	public void keyReleased(int k) {
+	}
+
+}
